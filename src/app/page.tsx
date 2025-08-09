7dfe4ba2eae3be8ecc251f58cf07073ab4ce1049
Fleming John <flemjohn08@gmail.com>
2025-08-09 06:57:12 +0000

    give sime animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { DevCard } from '@/components/dev-card';
 export default function Home() {
   return (
     <main className="relative flex min-h-screen w-full items-center justify-center p-4 overflow-hidden">
-      <div className="z-10">
+      <div className="z-10 animate-fade-in-scale">
         <DevCard />
       </div>
     </main>
