16a3a12dfc278a1637de5fa000130d85219549bb
Fleming John <flemjohn08@gmail.com>
2025-08-09 06:50:59 +0000

    oky remove this feature???

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import { DevCard } from '@/components/dev-card';
-import { ParticlesBackground } from '@/components/particles-background';
 
 export default function Home() {
   return (
     <main className="relative flex min-h-screen w-full items-center justify-center p-4 overflow-hidden">
-      <ParticlesBackground />
       <div className="z-10">
         <DevCard />
       </div>
