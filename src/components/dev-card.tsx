0c76479c5661b61500989e387721e31b937a0aa5
Fleming John <flemjohn08@gmail.com>
2025-08-09 06:24:42 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.div dat

--- src/components/dev-card.tsx
+++ src/components/dev-card.tsx
@@ -160,7 +160,7 @@ export function DevCard() {
                     ))}
                   </div>
               </TabsContent>
-              <TabsContent value="player-stats" className="mt-4 bg-black/20 p-4 rounded-md border border-primary/20">
+              <TabsContent value="player-stats" className="mt-4 bg-black/20 p-6 rounded-md border border-primary/20">
                 <PlayerStats stats={devData.stats} />
               </TabsContent>
             </Tabs>
@@ -208,4 +208,3 @@ export function DevCard() {
     </>
   );
 }
-
